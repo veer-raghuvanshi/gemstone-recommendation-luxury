3fdac16f8fd46a0af4947cc2b319bbdfa04e73e0
Veer Raghuvanshi <veerbeniyaaz0017@gmail.com>
2026-06-12 03:21:06 +0530

    fix build issues for vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    devSourcemap: false
+  },
+  build: {
+    cssMinify: false
+  }
 });
