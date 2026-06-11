d7ec15d2bfb149aef553051cf2ad3f4ea80f173c
Veer Raghuvanshi <veerbeniyaaz0017@gmail.com>
2026-06-12 03:16:29 +0530

    fix build dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()]
 });
